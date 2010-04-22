0f3da3cb792dccdaeae70384d5e22e5f0a612d66
Saikat Chakrabarti <saikat@gomockingbird.com>
2010-04-22 02:31:43 -0700

    Adding paperboy to be able to serve the client directly through node

--- server.js
+++ server.js
@@ -3,18 +3,49 @@ var http = require('http'),
     fs = require('fs'),
     io = require('./lib/socket.io'),
     sys = require('sys'),
+    paperboy = require('./lib/paperboy/paperboy'),
     
     send404 = function(res){
 	res.writeHead(404);
 	res.write('404');
 	res.close();
     },
-    
-    server = http.createServer(function(req, res){
-	send404(res);
-    });
 
+    /* This is copied exactly from the examply in node-paperboy - http://github.com/felixge/node-paperboy */
+    server = http.createServer(function(req, res) {
+	var ip = req.connection.remoteAddress;
+	paperboy
+	.deliver(__dirname, req, res)
+	.addHeader('Expires', 300)
+	.addHeader('X-PaperRoute', 'Node')
+	.before(function() {
+	    sys.log('Recieved Request')
+	})
+	.after(function(statCode) {
+	    res.write('Delivered: '+req.url);
+	    log(statCode, req.url, ip);
+	})
+	.error(function(statCode,msg) {
+	    res.writeHead(statCode, {'Content-Type': 'text/plain'});
+	    res.write("Error: " + statCode);
+	    res.close();
+	    log(statCode, req.url, ip, msg);
+	})
+	.otherwise(function(err) {
+	    var statCode = 404;
+	    res.writeHead(statCode, {'Content-Type': 'text/plain'});
+	    log(statCode, req.url, ip, err);
+	});
+    });
     server.listen(8080);
+
+    function log(statCode, url, ip,err) {
+	var logStr = statCode + ' - ' + url + ' - ' + ip
+	if (err)
+	    logStr += ' - ' + err;
+	sys.log(logStr);
+    }
+    /* End paperboy example */
     
     var json = JSON.stringify;
 
